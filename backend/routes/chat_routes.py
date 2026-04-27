deffead1c21a4a58c323fe0bb6538759c40810c1
BrendanH-Programmer <brendanhenderson@live.co.uk>
2026-04-27 13:43:00 +0100

    chat route fixed

--- backend/routes/chat_routes.py
+++ backend/routes/chat_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 
 chat_bp = Blueprint("chat", __name__)
 
-@chat_bp.route("/", methods=["POST"])
+@chat_bp.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
 
