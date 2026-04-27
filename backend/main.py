f72b11e565728e2278afdb417cad27c92da629d6
BrendanH-Programmer <brendanhenderson@live.co.uk>
2026-04-27 13:31:47 +0100

    frontend and backend updated

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+from routes.chat_routes import chat_bp
 import os
 
+# Set frontend folder path
 app = Flask(__name__, static_folder="../frontend")
 CORS(app)
+
+# Register API routes
+app.register_blueprint(chat_bp)
 
 print("Flask app created")
 
@@ -12,12 +17,10 @@ print("Flask app created")
 def home():
     return send_from_directory(app.static_folder, "index.html")
 
-
-# Serve any frontend file (css/js/images)
+# Serve static files (CSS, JS)
 @app.route("/<path:path>")
 def static_files(path):
     return send_from_directory(app.static_folder, path)
-
 
 if __name__ == "__main__":
     print("Starting Flask server...")
